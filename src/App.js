ac47c42b552e0a4a61ee22c24ed313aeb4a62f6e
jehfkemsy <jehf.denezaire@gmail.com>
2019-07-08 10:07:21 -0400

    added csv download and view to pregreg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import "./env";
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Landing from './pages/landing';
-import hackers from './pages/hackers';
-import Statistics from './pages/statistics'
-import Reminders from './pages/reminders';
-import Notifications from './pages/notifications';
-import PreRegistration from './pages/preReg'
+import Landing from "./pages/landing";
+import hackers from "./pages/hackers";
+import Statistics from "./pages/statistics";
+import Reminders from "./pages/reminders";
+import Notifications from "./pages/notifications";
+import PreRegistration from "./pages/preReg";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Route exact path="/" component={Landing} />
-        <Route exact path='/hackers' component={hackers} />
-        <Route exact path='/statistics' component={Statistics} />
-        <Route exact path='/reminders' component={Reminders} />
-        <Route exact path='/notifications' component={Notifications} />
+        <Route exact path="/hackers" component={hackers} />
+        <Route exact path="/statistics" component={Statistics} />
+        <Route exact path="/reminders" component={Reminders} />
+        <Route exact path="/notifications" component={Notifications} />
         <Route exact path="/preregistration" component={PreRegistration} />
       </Router>
     );
@@ -27,7 +26,6 @@ class App extends Component {
 }
 
 export default App;
-
 
 /**
  New page for reminders, live page, form for push notifications
