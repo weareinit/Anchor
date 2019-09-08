731ef4f14691eb3b90245945151d4ca410bd50ab
jehf denezaire <jehf.denezaire@gmail.com>
2019-09-07 21:09:41 -0400

    added option to download querry or all hackers as csv

--- src/pages/hackers/index.js
+++ src/pages/hackers/index.js
@@ -1,196 +1,225 @@
-import React,{Component,Fragment} from 'react';
-import {  CSVDownload } from "react-csv";
-import ReactPaginate from 'react-paginate';
+import React, { Component, Fragment } from "react";
+import { CSVLink } from "react-csv";
+import ReactPaginate from "react-paginate";
 
-import './style.css';
+import "./style.css";
 
-import Hacker from '../../components/hacker';
-import Navbar from '../../components/navbar';
-import ConfirmModal from '../../components/confirmModal';
+import Hacker from "../../components/hacker";
+import Navbar from "../../components/navbar";
+import ConfirmModal from "../../components/confirmModal";
 
-import Admin from '../../services/admin';
+import Admin from "../../services/admin";
 
-import getCSV from "../../utils/csvArr"
-import { get } from 'http';
+import { get } from "http";
 
-class Hackers extends Component{
-    constructor(props){
-        super(props)
+class Hackers extends Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            allApplicants: null,
-            applicants: null,
-            q: '',
-            page: 0,
-            overallPages: null,
-            count: null,
-            filter: '',
-            openModal: null,
-            shellIDs: null,
-            CSVArray:null,
-            gettingCSV: false
-        }
+    this.state = {
+      allApplicants: null,
+      applicants: null,
+      q: "",
+      page: 0,
+      overallPages: null,
+      count: null,
+      filter: "",
+      openModal: null,
+      shellIDs: null,
+      CSVArray: null,
+      gettingCSV: false
+    };
+  }
+
+  /**
+   * Initially calls applicants service with page 0 and no query
+   */
+  async componentDidMount() {
+    const { history } = this.props;
+
+    try {
+      const response = await Admin.getApplicants(0, null, history);
+      //console.log(response);
+      const { applicants, overallPages, count, allApplicants } = response;
+
+      this.setState({ count, overallPages, applicants, allApplicants });
+    } catch (e) {
+      console.log(e);
     }
+  }
 
-    
-    /**
-     * Initially calls applicants service with page 0 and no query
-     */
-    async componentDidMount(){
-        const{history} = this.props;
-        
-        try{
-        const response = await Admin.getApplicants(0,null,history);
-        //console.log(response);
-        const{applicants,overallPages,count,allApplicants} = response;
+  /**
+   * Calls applicants service with a query string
+   */
+  hackerSearch = async () => {
+    const { q, filter } = this.state;
+    const { history } = this.props;
 
-        this.setState({count,overallPages,applicants,allApplicants});
-       
-        }catch(e){
-            console.log(e);
-        }
+    try {
+      const response = await await Admin.getApplicants(0, q, history, filter);
+      const { applicants, overallPages, count, allApplicants } = response;
+
+      this.setState({
+        count,
+        overallPages,
+        applicants,
+        allApplicants,
+        page: 0
+      });
+    } catch (e) {
+      console.log(e);
     }
+  };
 
-   /**
-    * Calls applicants service with a query string
-    */
-    hackerSearch = async () => { 
-       const{q,filter} = this.state;
-       const{history} = this.props;
+  /**
+   * Calls applicants service with page number
+   * @param {data} data - Value from react-paginate
+   */
+  handlePageClick = async data => {
+    const { history } = this.props;
+    const { selected } = data;
+    const { q, filter } = this.state;
 
-       try{
-           const response = await await Admin.getApplicants(0,q,history,filter);
-           const{applicants,overallPages,count,allApplicants} = response
+    const response = await Admin.getApplicants(selected, q, history, filter);
+    const { applicants, allApplicants } = response;
 
-        this.setState({count,overallPages,applicants,allApplicants,page:0})
+    this.setState({ applicants, allApplicants });
+  };
 
-       }catch(e){
-           console.log(e);
-       }
-       
-    }
+  acceptAll = async () => {
+    const { allApplicants } = this.state;
+    let shellIDs = [];
 
-    /**
-     * Calls applicants service with page number
-     * @param {data} data - Value from react-paginate
-     */
-    handlePageClick = async data => {
-        const{history} = this.props;
-        const{selected} = data;
-        const{q,filter} = this.state;
+    allApplicants.map(applicant => {
+      const { shellID } = applicant;
+      shellIDs.push(shellID);
+    });
 
-        const response = await Admin.getApplicants(selected,q,history,filter);
-        const{applicants,allApplicants} = response
+    this.setState({ shellIDs, openModal: true });
+  };
 
-        this.setState({applicants,allApplicants});
-    }
+  handleInputChange(property) {
+    return e => {
+      this.setState({
+        [property]: e.target.value
+      });
+    };
+  }
 
-    acceptAll = async () => {
-        const{allApplicants} = this.state;
-        let shellIDs = [];
+  toggleChange = event => {
+    const { value } = event.target;
 
-        allApplicants.map(applicant => {
-            const {shellID} = applicant;
-            shellIDs.push(shellID);
-        })
+    this.setState({ filter: value });
+  };
 
-        this.setState({shellIDs, openModal: true});
-    }
-
-    handleInputChange(property) {
-        return e => {
-          this.setState({
-            [property]: e.target.value
-          });
-        };
-      }
-
-    toggleChange = (event) => {
-        const{value} = event.target
-
-        this.setState({filter:value});
-    }
-
-
-    getDataAsync = async()=>{
-        this.setState({gettingCSV:true});
-
-        const data = await getCSV(this.applicants)
-
-        this.setState({CSVArray:data,gettingCSV:false});
-    }
-
-    render(){
-        const{applicants,overallPages,count,openModal,shellIDs} = this.state;
-        const{history} = this.props;
-        this.getDataAsync()
-        return(
-            applicants ?
-            <div>
-                <Navbar />
-                <div
-          style={{
-            width: "80%",
-            display: "flex",
-            justifyContent: "space-between"
-          }}
-        >
-            {this.state.gettingCSV?
-            <p>Processing CSV data</p>
-            :  
-            <CSVDownload
-            style={{ width: "40%", padding: "8%", background: "#A2977D" }}
-            data={this.state.CSVArray }
-            target="_blank"
-        >
-            Download Current Querrry CSV
-           </CSVDownload>
-            }
-                    </div>
-                    <div className="hackerOuter">
-                    <ConfirmModal close = {() => this.setState({openModal:false})} action={() => Admin.acceptHacker(shellIDs)} open ={openModal} description = {`accept all ${count} hackers`} />
-                        <input 
-                        onChange = {this.handleInputChange('q')} 
-                        placeholder="Search for hacker" 
-                        className="hackerInput" 
-                        type='text'
-                        />
-                        <br />
-                        <div onChange={this.toggleChange} className="filters">
-                            <label><input defaultChecked value={null} name="hackerFilter" className="toggle" type="radio"/> All</label>
-                            <label><input name="hackerFilter" value="applied" className="toggle" type="radio"/> Applied</label>
-                            <label><input name="hackerFilter" value="accepted" className="toggle" type="radio"/> Accepted</label>
-                            <label><input name="hackerFilter" value="confirmed" className="toggle" type="radio"/> Confirmed</label>
-                        </div>
-                        <button onClick = {this.hackerSearch} className="searchBtn">Search</button>
-                        <h2>{count} Hackers Found</h2>
-                        <button onClick={this.acceptAll} className="allBtn">Accept All</button>
-                        <div className="hackersContainer">
-                            {applicants.map(hacker => {
-                                return <Hacker history={history} data = {hacker} />
-                            })}
-                        </div>
-                        <ReactPaginate
-                        previousLabel={'previous'}
-                        nextLabel={'next'}
-                        breakLabel={'...'}
-                        breakClassName={'break-me'}
-                        pageCount={overallPages}
-                        marginPagesDisplayed={2}
-                        pageRangeDisplayed={5}
-                        onPageChange={this.handlePageClick}
-                        containerClassName={'react-paginate'}
-                        subContainerClassName={'pages pagination'}
-                        activeClassName={'active'}
-                        />
-                    </div>
-            </div>
-            :
-            <div className="hackerOuter">
-                <h1 id="loading">Loading...</h1>
-            </div>
-        );
-    }
+  render() {
+    const { applicants, overallPages, count, openModal, shellIDs } = this.state;
+    const { history } = this.props;
+    return applicants ? (
+      <div>
+        <Navbar />
+        <div className="hackerOuter">
+          <ConfirmModal
+            close={() => this.setState({ openModal: false })}
+            action={() => Admin.acceptHacker(shellIDs)}
+            open={openModal}
+            description={`accept all ${count} hackers`}
+          />
+          <input
+            onChange={this.handleInputChange("q")}
+            placeholder="Search for hacker"
+            className="hackerInput"
+            type="text"
+          />
+          <br />
+          <div onChange={this.toggleChange} className="filters">
+            <label>
+              <input
+                defaultChecked
+                value={null}
+                name="hackerFilter"
+                className="toggle"
+                type="radio"
+              />{" "}
+              All
+            </label>
+            <label>
+              <input
+                name="hackerFilter"
+                value="applied"
+                className="toggle"
+                type="radio"
+              />{" "}
+              Applied
+            </label>
+            <label>
+              <input
+                name="hackerFilter"
+                value="accepted"
+                className="toggle"
+                type="radio"
+              />{" "}
+              Accepted
+            </label>
+            <label>
+              <input
+                name="hackerFilter"
+                value="confirmed"
+                className="toggle"
+                type="radio"
+              />{" "}
+              Confirmed
+            </label>
+          </div>
+          <button onClick={this.hackerSearch} className="searchBtn">
+            Search
+          </button>
+          <div className="csv-container">
+            <CSVLink
+              className="csv-button"
+              data={this.state.allApplicants}
+              target="_blank"
+            >
+              Download all Applicants
+            </CSVLink>
+            <CSVLink
+              className="csv-button"
+              data={this.state.applicants}
+              target="_blank"
+            >
+              Download Querry
+            </CSVLink>
+          </div>
+          <h2>{count} Hackers Found</h2>
+          <button onClick={this.acceptAll} className="allBtn">
+            Accept All
+          </button>
+          <div className="hackersContainer">
+            {applicants.map(hacker => {
+              return <Hacker history={history} data={hacker} />;
+            })}
+          </div>
+          <ReactPaginate
+            previousLabel={"previous"}
+            nextLabel={"next"}
+            breakLabel={"..."}
+            breakClassName={"break-me"}
+            pageCount={overallPages}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={this.handlePageClick}
+            containerClassName={"react-paginate"}
+            subContainerClassName={"pages pagination"}
+            activeClassName={"active"}
+          />
+        </div>
+      </div>
+    ) : (
+      <div className="hackerOuter">
+        <h1 id="loading">Loading...</h1>
+      </div>
+    );
+  }
 }
 
 export default Hackers;
